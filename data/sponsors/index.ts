e465eaccb36aeb912fa840c8adbc12f1b0b40083
Paul Leblanc <paul.leblanc22@gmail.com>
2026-08-27 18:32:54 +0200

    feat: add country sponsor batch 04

--- data/sponsors/index.ts
+++ data/sponsors/index.ts
@@ -11,6 +11,7 @@ import { CONFECTIONERY_SPONSORS } from "./confectionery";
 import { COUNTRY_EXPANSION_BATCH_01_SPONSORS } from "./country-expansion-batch-01";
 import { COUNTRY_EXPANSION_BATCH_02_SPONSORS } from "./country-expansion-batch-02";
 import { COUNTRY_EXPANSION_BATCH_03_SPONSORS } from "./country-expansion-batch-03";
+import { COUNTRY_EXPANSION_BATCH_04_SPONSORS } from "./country-expansion-batch-04";
 import { CYCLING_PROJECT_SPONSORS } from "./cycling-projects";
 import { ENERGY_SPONSORS } from "./energy";
 import { FAST_FOOD_SPONSORS } from "./fast-food";
@@ -43,6 +44,7 @@ const RAW_SPONSORS = [
   ...COUNTRY_EXPANSION_BATCH_01_SPONSORS,
   ...COUNTRY_EXPANSION_BATCH_02_SPONSORS,
   ...COUNTRY_EXPANSION_BATCH_03_SPONSORS,
+  ...COUNTRY_EXPANSION_BATCH_04_SPONSORS,
   ...CONFECTIONERY_SPONSORS,
   ...ENERGY_SPONSORS,
   ...BANKING_SPONSORS,
