253158ec2fb9969c56c824236bfc6d0ec3fb009b
Paul Leblanc <paul.leblanc22@gmail.com>
2026-09-19 17:07:46 +0200

    fix: publish permanent Discord invite

--- components/layout/public-footer.test.tsx
+++ components/layout/public-footer.test.tsx
@@ -13,7 +13,7 @@ describe("PublicFooter", () => {
   it("affiche les accès Discord et Instagram avec leurs logos", () => {
     const markup = renderToStaticMarkup(<PublicFooter />);
 
-    expect(markup).toContain("https://discord.gg/Zq9ecPYEF");
+    expect(markup).toContain("https://discord.gg/EbupEFEQC8");
     expect(markup).toContain("https://www.instagram.com/cyclostratege/");
     expect(markup).toContain("/images/social/discord-symbol.svg");
     expect(markup).toContain("/images/social/instagram-glyph.svg");
